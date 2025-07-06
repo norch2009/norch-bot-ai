817e5212f91b1e80fbfb83bce82065fac5b3e419
norch2009 <norch853@gmail.com>
2025-07-06 18:19:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require("express");
+const app = express();
 const path = require("path");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+// Serve static files like script.js, style.css, index.html
+app.use(express.static("public"));
 
-// Serve static files from "public" folder
-app.use(express.static(path.join(__dirname, "public")));
-
-// Root route
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`✅ Norch UI running at http://localhost:${PORT}`);
-});
+app.listen(3000, () => console.log("Norch UI running on http://localhost:3000"));
